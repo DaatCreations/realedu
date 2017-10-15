825b132adbce1a63d2c74b8d9fc772571fb253b2
Davinderpal Singh Rehal <davinderpalrehal@gmail.com>
2017-10-15 16:35:07 +0300

    Got HTML working again
    Signed-off-by: Davinderpal Singh Rehal <davinderpalrehal@gmail.com>

--- functions.php
+++ functions.php
@@ -118,6 +118,7 @@ add_action( 'widgets_init', 'realedu_widgets_init' );
  */
 function realedu_scripts() {
 	wp_enqueue_style( 'realedu-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'realedu-app', get_template_directory_uri() . '/css/app.css' );
 
 	wp_enqueue_script( 'realedu-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
